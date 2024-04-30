518be09ed8cc7ae73391fd32cc232444895df75e
COD-SAMURAI <darshany409@gmail.com>
2024-04-30 23:16:34 +0530

    Home slider working sucessfully

--- Home.js
+++ Home.js
@@ -5,7 +5,7 @@ function redirectToCollege() {
 
 
 var Time=5000;
-var slideIndex = 0; // Start from the first slide
+var slideIndex = 0;
 showSlides();
 
 function showSlides() {
@@ -13,31 +13,31 @@ function showSlides() {
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
     
-    // Hide all slides and remove active dot class
+
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
         dots[i].className = dots[i].className.replace(" active", "");
     }
     
-    // Increment slideIndex and reset to 1 if it exceeds the number of slides
+
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1; }  
     
-    // Display the current slide and set active dot class
+
     slides[slideIndex - 1].style.display = "block";  
     dots[slideIndex - 1].className += " active";
     
-    // Call the function recursively every 5 seconds
+
     setTimeout(showSlides, Time);
 }
 
-// Function to advance to the next slide manually
+
 function plusSlides(n) {
     slideIndex += n;
     showSlides();
 }
 
-// Function to jump to a specific slide
+
 function currentSlide(n) {
     slideIndex = n;
     showSlides();
