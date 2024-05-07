d6b5e37f0c2468d658ec9bca41d2e70b18c477c7
ABHISHEK.MANOHAR.B <bilagiabhishek56@gmail.com>
2024-05-07 20:47:50 +0530

    changes to home

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function redirectToCollege() {
 }
 
 
-var Time=5000;
+var Time = 5000;
 var slideIndex = 0;
 showSlides();
 
@@ -12,21 +12,21 @@ function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
-    
+
 
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    
+
 
     slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1; }  
-    
+    if (slideIndex > slides.length) { slideIndex = 1; }
 
-    slides[slideIndex - 1].style.display = "block";  
+
+    slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
-    
+
 
     setTimeout(showSlides, Time);
 }
@@ -41,4 +41,21 @@ function plusSlides(n) {
 function currentSlide(n) {
     slideIndex = n;
     showSlides();
+}
+
+function showBlog(id) {
+    var modal = document.getElementById("blog-modal");
+    var content = document.getElementById("blog-content");
+
+    // Here you can load the content of the respective blog based on its id
+    // For demonstration, let's assume we have preloaded content for each blog
+    var blogContent = ""; // Content of the respective blog
+
+    content.innerHTML = blogContent;
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    var modal = document.getElementById("blog-modal");
+    modal.style.display = "none";
 }
